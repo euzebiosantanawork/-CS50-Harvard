8d5ddfe6186167ebae277078dedeb7448d7e7dd2
Euzebio <euzebiosantanawork@gmail.com>
2020-04-12 15:04:51 -0300

    agree.c (1)

---
+++ agree.c
@@ -0,0 +1,15 @@
+int main(void)
+{
+    // Prompt user to agree
+    char c = get_char("Do you agree?\n");
+
+    // Check whether agreed
+    if (c == 'Y' || c == 'y')
+    {
+        printf("Agreed.\n");
+    }
+    else if (c == 'N' || c == 'n')
+    {
+        printf("Not agreed.\n");
+    }
+}
