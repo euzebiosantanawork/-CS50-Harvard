6a111d417b768a6ee1e8150c774a1ac366b33696
Euzebio <euzebiosantanawork@gmail.com>
2020-04-12 15:15:45 -0300

    cough3.c (1)

---
+++ cough3.c
@@ -0,0 +1,16 @@
+
+void cough(int n);
+
+int main(void)
+{
+    cough(3);
+}
+
+// Cough some number of times
+void cough(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("cough\n");
+    }
+}
