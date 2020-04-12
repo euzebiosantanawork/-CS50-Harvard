3794c3947eeed8ff548c84ea65d854126c3d29fb
Euzebio <euzebiosantanawork@gmail.com>
2020-04-12 15:07:53 -0300

    conditions.c (1)

---
+++ conditions.c
@@ -0,0 +1,15 @@
+
+    // Compare x and y
+    if (x < y)
+    {
+        printf("x is less than y\n");
+    }
+    else if (x > y)
+    {
+        printf("x is greater than y\n");
+    }
+    else
+    {
+        printf("x is equal to y\n");
+    }
+}
